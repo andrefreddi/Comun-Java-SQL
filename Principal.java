cfb0bdb875c1b31e1737b096cb4688d2d1d8abb3
andre.freddi <52337680+andrefreddi@users.noreply.github.com>
2020-06-19 02:10:22 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -9,77 +9,281 @@ import java.sql.Statement;
 
 public class Principal {
 
+	//Conceito principal!! Ter um CD, Artista e Produtora, Artisa e Produtora se ligam a CD
+	//Cada classe possui 3 elementos, Principal.java executa eles fazendo a conexcao ao banco
+	//Ocorre uma busca SQL no aplicativo Java
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// Cria variável p/ string de conexão
-        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=ExemploBanco;user=exemplobanco;password=24685";
+		
+		//Conexao
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Avaliacaon2;user=Avaliacaon2;password=24685";
         
-        // String com a consulta de inserção - ? = variável 
-        String insertString = "INSERT INTO Pessoa (id, nome, idade) VALUES (?, ?, ?)";
+        //Set
         
-        // Tentativa de inserir valor na tabela
+        String insertString = "INSERT INTO Artista (id_artista, nome_artista) VALUES (?, ?)";
+        String insertString1 = "INSERT INTO CD (id_cd, nome_cd, id_artista, id_produtora) VALUES (?, ?, ?, ?)";
+        String insertString2 = "INSERT INTO Produtora (id_produtora, nome_produtora) VALUES (?, ?)";
+        
+        //ABAIXO A CRIACAO DE ARTISTAS
+        
+        	// Adicionar a tabela o Artita 01
         try (
         		Connection con = DriverManager.getConnection(connectionUrl); 
         		PreparedStatement stmt = con.prepareStatement(insertString);
         	) {
-        	Pessoa p1 = new Pessoa(3, "Andre", 50);
-        	// insira na primeira ? o id da pessoa
-        	stmt.setInt(1, p1.getId());
-        	stmt.setString(2, p1.getNome()); // insira na segunda ? o nome da pessoa
-        	stmt.setInt(3, p1.getIdade()); // insira na terceira ? a idade da pessoa
         	
-        	stmt.executeUpdate(); // executa o insert
+        	Artista artista1 = new Artista(1, "Acxle");
         	
-        	Pessoa p2 = new Pessoa(4, "luciano", 24);
-        	// insira na primeira ? o id da pessoa
-        	stmt.setInt(1, p2.getId());
-        	stmt.setString(2, p2.getNome()); // insira na segunda ? o nome da pessoa
-        	stmt.setInt(3, p2.getIdade()); // insira na terceira ? a idade da pessoa
+        	//==============================
+        	stmt.setInt(1, artista1.getId_artista());
+        	stmt.setString(2, artista1.getNome_artista());
+        	//==============================
         	
-        	stmt.executeUpdate(); // executa o insert 
+        	stmt.executeUpdate();
+        
+        	//==============================
 
         } catch (SQLException e) {
         	e.printStackTrace();
         }
         
+    	// Adicionar a tabela o Artita 02
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString);
+    	) {
+    	
+    	
+    	// Adicionar a tabela o Artita 02
+    	Artista artista2 = new Artista(2, "Joji");
+    	
+    	//==============================
+    	stmt.setInt(1, artista2.getId_artista());
+    	stmt.setString(2, artista2.getNome_artista());
+    	//==============================
+    	
+    	stmt.executeUpdate();
+
+    	} catch (SQLException e) {
+    	e.printStackTrace();
+    	}
+    
+		// Adicionar a tabela o Artita 03
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString);
+    	) {
+	
+	
+    	// Adicionar a tabela o Artita 03
+    	Artista artista3 = new Artista(3, "N'To");
+	
+    	//==============================
+    	stmt.setInt(1, artista3.getId_artista());
+    	stmt.setString(2, artista3.getNome_artista());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    }
+        
+  //ABAIXO A CRIACAO DE CDs
+    
+	// Adicionar a tabela o CD 01
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString1);
+    	) {
+	
+	
+    	// Adicionar a tabela o CD 01
+    	CD cd1 = new CD(1, "faces", 1, 1);
+	
+    	//==============================
+    	stmt.setInt(1, cd1.getId_cd());
+    	stmt.setString(2, cd1.getNome_cd());
+    	stmt.setInt(3, cd1.getId_artista());
+    	stmt.setInt(4, cd1.getId_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    } 
+    
+	// Adicionar a tabela o CD 02
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString1);
+    	) {
+	
+	
+    	// Adicionar a tabela o CD 02
+    	CD cd2 = new CD(2, "Gimme Love", 2, 2);
+	
+    	//==============================
+    	stmt.setInt(1, cd2.getId_cd());
+    	stmt.setString(2, cd2.getNome_cd());
+    	stmt.setInt(3, cd2.getId_artista());
+    	stmt.setInt(4, cd2.getId_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    } 
+    
+	// Adicionar a tabela o CD 03
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString1);
+    	) {
+	
+	
+    	// Adicionar a tabela o CD 03
+    	CD cd3 = new CD(3, "La Clé Des Champs", 3, 3);
+	
+    	//==============================
+    	stmt.setInt(1, cd3.getId_cd());
+    	stmt.setString(2, cd3.getNome_cd());
+    	stmt.setInt(3, cd3.getId_artista());
+    	stmt.setInt(4, cd3.getId_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    } 
+    
+    
+    
+ // Adicionar a tabela a produtora 01
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString2);
+    	) {
+	
+	
+    	// Adicionar a tabela a produtora 01
+    	Produtora produtora1 = new Produtora(1, "Inde");
+	
+    	//==============================
+    	stmt.setInt(1, produtora1.getId_produtora());
+    	stmt.setString(2, produtora1.getNome_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    } 
+    
+    
+    // Adicionar a tabela a produtora 02
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString2);
+    	) {
+	
+	
+    	// Adicionar a tabela a produtora 02
+    	Produtora produtora2 = new Produtora(2, "Pink Guy");
+	
+    	//==============================
+    	stmt.setInt(1, produtora2.getId_produtora());
+    	stmt.setString(2, produtora2.getNome_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    } 
+    
+    
+    // Adicionar a tabela a produtora 03
+    try (
+    		Connection con = DriverManager.getConnection(connectionUrl); 
+    		PreparedStatement stmt = con.prepareStatement(insertString2);
+    	) {
+	
+	
+    	// Adicionar a tabela a produtora 03
+    	Produtora produtora3 = new Produtora(3, "Hungry Music");
+	
+    	//==============================
+    	stmt.setInt(1, produtora3.getId_produtora());
+    	stmt.setString(2, produtora3.getNome_produtora());
+    	//==============================
+	
+    	stmt.executeUpdate();
+
+    } catch (SQLException e) {
+    	e.printStackTrace();
+    }
+    
+    
+
         // faz um SELECT no banco
         try (
         		Connection con = DriverManager.getConnection(connectionUrl); 
-        		Statement stmt = con.createStatement(); // não tem variável, se quiser variável, precisa do prepare
+        		Statement stmt = con.createStatement();
         	) {
         	
-            String SQL = "SELECT * FROM Pessoa"; // consulta de SELECT
-            ResultSet rs = stmt.executeQuery(SQL); // executa o SELECT
+        	//Consulta SQL
+            String SQL = "select *  from CD  join Artista on CD.id_artista = Artista.id_artista";
+            ResultSet rs = stmt.executeQuery(SQL); 
 
-            // itera pelos dados - enquanto houver uma linha para ler ....
+            
             while (rs.next()) {
-            	Pessoa p = new Pessoa(); // cria um objeto de pessoa
-            	p.setId(rs.getInt("id")); // insere id recuperado do banco na pessoa
-            	p.setNome(rs.getString("nome")); // insere nome recuperado do banco na pessoa
-            	p.setIdade(rs.getInt("idade")); // insere idade recuperada do banco na pessoa
+            	
+            	//Set
+            	Artista artista = new Artista();
+            	artista.setId_artista(rs.getInt("id_artista"));
+            	artista.setNome_artista(rs.getString("nome_artista"));
+            	
+            	CD cd = new CD();
+            	cd.setId_cd(rs.getInt("id_cd"));
+            	cd.setNome_cd(rs.getString("nome_cd"));
+            	cd.setId_artista(rs.getInt("id_artista"));
+            	cd.setId_produtora(rs.getInt("id_produtora"));
 
-            	System.out.println(p); // imprime a pessoa
+            	System.out.println(artista);
+            	System.out.println(cd);
             }
         }
-        // Handle any errors that may have occurred.
+        
         catch (SQLException e) {
             e.printStackTrace();
         }
         
-        String updateString = "UPDATE Pessoa SET nome = ?, idade = ? WHERE id=?";
         
-        // Tentativa de atualizar valor na tabela
+      //===================================
+        
+       
+     // UPDATE NO ID 1 DA PRODUTORA PARA ALTERAR DE "INDE" PARA "INDEPOP"   
+        
+      /*
+        String updateString = "UPDATE Produtora WHERE id_produtora = ?";
+        
         try (
         		Connection con = DriverManager.getConnection(connectionUrl); 
         		PreparedStatement stmt = con.prepareStatement(updateString);
         	) {
         	
-        	Pessoa p1 = new Pessoa(1, "Mariano", 20); // a pessoa alterada
-        	stmt.setString(1, p1.getNome()); // insira na primeira ? o nome da pessoa
-        	stmt.setInt(2, p1.getIdade()); // insira na terceira ? a idade da pessoa
-        	// insira na última ? o id da pessoa
-        	stmt.setInt(3, p1.getId());	
+        	Produtora produtora1 = new Produtora(1, "IndePop");
+        	stmt.setInt(1, produtora1.getId_produtora());
+        	stmt.setString(2, produtora1.getNome_produtora());	
         	
-        	stmt.executeUpdate(); // executa o update
+        	stmt.executeUpdate(); 
         	
         	System.out.println("Pessoa alterada!");
         	
@@ -87,19 +291,21 @@ public class Principal {
         	e.printStackTrace();
         }
         
-        String deleteString = "DELETE Pessoa WHERE id=?";
+        */
         
-        // Tentativa de remover valor na tabela
+        // ISSO IRA REMOVER O PRODUTORA DE ID 1 "INDE"
+        
+        String deleteString = "DELETE Produtora WHERE id_produtora=?";
+        
         try (
         		Connection con = DriverManager.getConnection(connectionUrl); 
         		PreparedStatement stmt = con.prepareStatement(deleteString);
         	) {
         	
-        	int personId = 2;
-        	// insira na ? o id da pessoa
-        	stmt.setInt(1, personId);
+        	int Id_produtora = 1;
+        	stmt.setInt(1, Id_produtora);
         	
-        	stmt.executeUpdate(); // executa o delete
+        	stmt.executeUpdate();
         	
         	System.out.println("Pessoa removida!");
         	
@@ -107,63 +313,5 @@ public class Principal {
         	e.printStackTrace();
         }
         
-        
-     // faz um SELECT no banco
-        try (
-        		Connection con = DriverManager.getConnection(connectionUrl); 
-        		Statement stmt = con.createStatement(); // não tem variável, se quiser variável, precisa do prepare
-        	) {
-        	
-            String SQL = "SELECT * FROM Pessoa"; // consulta de SELECT
-            ResultSet rs = stmt.executeQuery(SQL); // executa o SELECT
-
-            // itera pelos dados - enquanto houver uma linha para ler ....
-            while (rs.next()) {
-            	Pessoa p = new Pessoa(); // cria um objeto de pessoa
-            	p.setId(rs.getInt("id")); // insere id recuperado do banco na pessoa
-            	p.setNome(rs.getString("nome")); // insere nome recuperado do banco na pessoa
-            	p.setIdade(rs.getInt("idade")); // insere idade recuperada do banco na pessoa
-
-            	System.out.println(p); // imprime a pessoa
-            }
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        // Tentativa de inserir valor na tabela
-        // Tentativa de adicionar o Emprego de cada pessoa
-        try (
-        		Connection con = DriverManager.getConnection(connectionUrl); 
-        		PreparedStatement stmt = con.prepareStatement(insertString);
-        	) {
-        	Emprego e1 = new Emprego(1, "Embraco", 1);
-        
-        	stmt.setInt(1, e1.getId());	// insira o id da pessoa
-        	stmt.setString(2, e1.getNome()); // insira o nome da empresa
-        	stmt.setInt(3, e1.getidemprego()); // insira o id da empresa
-        	
-        	stmt.executeUpdate(); // executa o insert
-	} catch (SQLException e) {
-    	e.printStackTrace();
-    }
-        
-        // Tentativa de inserir valor na tabela
-        // Tentativa de adicionar o local da empresa de cada pessoa
-        try (
-        		Connection con = DriverManager.getConnection(connectionUrl); 
-        		PreparedStatement stmt = con.prepareStatement(insertString);
-        	) {
-        	LocalEmpresa l1 = new LocalEmpresa(1, "ruatallugar");
-        	
-        	stmt.setInt(1, l1.getidemprego()); // insira id da empresa
-        	stmt.setString(2, l1.getlocal()); // insira local da empresa
-        	
-        	stmt.executeUpdate(); // executa o insert
-	} catch (SQLException e) {
-    	e.printStackTrace();
-    }
-
-}
+	}
 }
